68e5c49d77b8ee5b5b3bda3710cd000ea89a948b
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2017-10-22 17:35:35 +0400

    submit?phone=asdf

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 use infrajs\rest\Rest;
 use infrajs\ans\Ans;
-
+use infrajs\template\Template;
+use infrajs\mail\Mail;
 
 Rest::get( function () {
 	echo '<h1>Заказать звонок</h1>';
@@ -10,6 +11,11 @@ Rest::get( function () {
 	echo '<p><a href="http://lawfortis.ru.org/-flashing-callback/submit?phone=234234">Проверочная ссылка</a></p>';
 },'submit', function () {
 	$ans = array();
-	$ans['phone'] = Ans::REQ('phone');
-	return Ans::ret($ans,'Телефонный номер <b>'.$ans['phone'].'</b> принят. Менеджер свяжется с вами в рабочее время');
+	$ans['phone'] = Ans::REQ('phone','string','');
+	if (strlen($ans['phone'])<2) return Ans::err($ans,'Ошибка. Указан некорректный телефон.');
+
+	$body = Template::parse('-flashing-callback/mail.tpl',$ans);
+	Mail::toAdmin('Телефон с сайта '.$_SERVER['HTTP_HOST'], 'noreplay@'.$_SERVER['HTTP_HOST'], $body);
+
+	Ans::ret($ans,'Телефонный номер <b>'.$ans['phone'].'</b> принят.');
 });

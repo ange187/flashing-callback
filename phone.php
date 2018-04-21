55638a409d4111c444e787d9ec2d83584c65a55d
Vika <v.vinichuk@itlf.ru>
2018-04-21 20:18:26 +0400

    fix

--- phone.php
+++ phone.php
@@ -13,6 +13,12 @@ $ans = array();
 $ans['phone'] = $phone;
 if (strlen($phone) < 6 ) return Ans::err($ans,'Уточните ваш телефон');
 
+session_start();
+if (!isset($_SESSION['submit_time'])) $_SESSION['submit_time'] = 0;
+ 
+if (time() - $_SESSION['submit_time'] < 60) return Ans::err($ans, 'Письмо уже отправлено! Новое сообщение можно будет отправить через 1 минуту!');
+$_SESSION['submit_time'] = time();
+
 $subject = 'Заказ обратного звонка';
 $body = 'Перезвоните по телефону\n'.$phone;
 $from = "noreplay@".$_SERVER["HTTP_HOST"];
